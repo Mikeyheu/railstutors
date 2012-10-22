c453073c6c451b1f4ee08376a90a1e6b2da67751
Michael Heu <mheu@clickbooq.com>
2012-10-22 16:07:53 -0400

    twitter feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(params[:user])
 		if @user.save
-      redirect_to posts_path, :notice => "Signed up!"
+      redirect_to root_path, :notice => "Signed up!"
     else
       render "new"
     end
@@ -15,5 +15,12 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		# @tweets = Twitter.user_timeline(@user.twitter_handle).first(10)
+
+		@client = Twitter::Client.new( 
+			:consumer_key => 'HvNG5vT62tkzCWfc7W181w', 
+			:consumer_secret => '3OFejsKbtpaLGpxWGwsYjk82AOrbJ1rjouqV6mRUT0', 
+			:oauth_token => "892169029-tZZiA96BCKFKpfoiFeBq6gNOM6QdNhMUcQg0UVWB", 
+			:oauth_token_secret => "nLukXV8VBI5BaYhxVMndkv1Su4XOB73aJpFkNZ9Odc" 
+			) 
 	end
 end
